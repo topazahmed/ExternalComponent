53b046719236fe977039f979aa3529eccdb0d0c1
topazahmed <thisistopaz@hotmail.com>
2023-04-14 13:53:06 -0600

    main structure

---
+++ README.md
@@ -0,0 +1,4 @@
+1. make sure you are in the right folder 
+1. run  `npm run build --component=chga`
+1. this will create the js package inside the dist folder
+1. run `npm run dev` to run locally
--- src/components/chga/index.tsx
+++ src/components/chga/index.tsx
@@ -1,55 +1,110 @@
-import React from "react";
-import ReactDOM from "react-dom";
+//import React from "react";
+//import ReactDOM from "react-dom";
 
-const OptionsContext = React.createContext<any>(null);
-
-// const onEntitySaved = (evt: Event): void => {
-//     const { definitionName, id } = (evt as CustomEvent<{ definitionName: string; id: number }>).detail;
-
-//     alert(`Entity with id ${id} and definition ${definitionName} was saved`);
-// };
-
-//const EVENT_NAME = "ENTITY_SAVED";
-declare global{
-    interface Window {dataLayer:any}
+//const OptionsContext = React.createContext<any>(null);
+declare global {
+  interface Window {
+    dataLayer: any;
+  }
 }
 
-export default function createExternal (container:any) {
-    let eventHandler: EventListenerOrEventListenerObject | null = null;
+export default function createExternal(container: any) {
+  let shareLinkEventHandler: EventListenerOrEventListenerObject | null = null;
+  let createLinkEventHandler: EventListenerOrEventListenerObject | null = null;
+  let copyLinkEventHandler: EventListenerOrEventListenerObject | null = null;
+  let shareEmailEventHandler: EventListenerOrEventListenerObject | null = null;
 
-    const removeListener = () => {
-        if (eventHandler) {
-            //window.removeEventListener(EVENT_NAME, eventHandler);
-            eventHandler = null;
-        }
-    };
-    console.log("We are in here");
+  const removeListener = () => {
+    if (shareLinkEventHandler) {
+      shareLinkEventHandler = null;
+      createLinkEventHandler = null;
+      copyLinkEventHandler = null;
+      shareEmailEventHandler = null;
+    }
+  };
 
-    return {
-        render: (context: { options: { entityId: any; }; }) => {
-            //removeListener();
-            //var dataLayer:any = dataLayer || [];
-            eventHandler = event => {
-                console.log("button clicked");
-                //window && window.dataLayer && 
-                window.dataLayer.push({
-                    event: "sharePublicCollection",
-                    //entityId: context.options.entityId,
-                    definitionName: 'sharePublicCollection', //event.detail.definitionName,
-                    description: 'share Public Collection',
-                    collectionId: '111'
-                });
+  console.log("Custom external react component loaded!");
+  var collectionId = location.pathname.split("/").pop();
+  console.log("Collection id: " + collectionId);
 
-                console.log(window.dataLayer);
-            }
+  return {
+    render: (context: { options: { entityId: any } }) => {
+      removeListener();
 
-            //window.addEventListener(EVENT_NAME, eventHandler);
-            const button = document.querySelectorAll("[data-testid='sharePublicCollection']")[0];
-            button?.addEventListener("click", eventHandler);
+      shareLinkEventHandler = (event) => {
+        console.log("Share link button clicked");
+        //window && window.dataLayer &&
+        window.dataLayer.push({
+          event: "sharePublicCollection",
+          //entityId: context.options.entityId,
+          definitionName: "sharePublicCollection", //event.detail.definitionName,
+          description: "share Public Collection",
+          collectionId: collectionId,
+        });
+        //console.log(window.dataLayer);
+      };
 
-        },
-        unmount: () => {
-            removeListener();
-        }
-    };
-};
+      createLinkEventHandler = (event) => {
+        console.log("Create external link checkbox clicked");
+        window.dataLayer.push({
+          event: "createExternalLinkPublicCollection",
+          definitionName: "createExternalLinkPublicCollection", //event.detail.definitionName,
+          description: "create external link for Public Collection",
+          collectionId: collectionId,
+        });
+      };
+
+      copyLinkEventHandler = (event) => {
+        console.log("Copy external link button clicked");
+        window.dataLayer.push({
+          event: "copyLinkPublicCollection",
+          definitionName: "copyLinkPublicCollection", //event.detail.definitionName,
+          description: "copy external link for Public Collection",
+          collectionId: collectionId,
+        });
+      };
+
+      shareEmailEventHandler = (event) => {
+        console.log("Email button clicked");
+        window.dataLayer.push({
+          event: "emailPublicCollection",
+          //entityId: context.options.entityId,
+          definitionName: "emailPublicCollection", //event.detail.definitionName,
+          description: "email link for Public Collection",
+          collectionId: collectionId,
+        });
+        //console.log(window.dataLayer);
+      };
+
+      document
+        .querySelectorAll("[data-testid='sharePublicCollection']")[0]
+        ?.addEventListener("click", shareLinkEventHandler);
+
+      //   const headerButton = document.querySelectorAll(
+      //     "[data-testid='sharePublicCollection']"
+      //   )[0];
+      //   headerButton?.addEventListener("click", shareLinkEventHandler);
+
+      const createExternalLinkButton = document.getElementById(
+        collectionId + "-Create-external-link"
+      );
+      createExternalLinkButton?.addEventListener(
+        "change",
+        createLinkEventHandler
+      );
+
+      const copyLinkButton = document.querySelectorAll(
+        "[data-testid='copy-external-link']"
+      )[0];
+      copyLinkButton?.addEventListener("click", copyLinkEventHandler);
+
+      const shareEmailButton = document.querySelectorAll(
+        "[data-testid='share-via-email']"
+      )[0];
+      shareEmailButton?.addEventListener("click", shareEmailEventHandler);
+    },
+    unmount: () => {
+      removeListener();
+    },
+  };
+}
